707da08ff600a3945919f6ae9872eb382fac33d2
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-05-13 15:23:05 -0400

    finished tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('#create-task-form')
+  let formInput = document.querySelector('#new-task-description')
+  let taskList = document.querySelector('#tasks')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(formInput.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn. addEventListener('click', handleDelete)
+  btn.textContent = 'x';
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector(`#tasks`).appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
